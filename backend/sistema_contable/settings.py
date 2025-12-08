1d41dd7679902a003950b65e77412186628acfb4
Framncisco Salamanca <Framncisco@gmail.com>
2025-12-08 10:59:01 -0600

    Mejorar la concion con rain en settings

--- backend/sistema_contable/settings.py
+++ backend/sistema_contable/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-!(2sc4w)13(5ev2szrmeg7c*k85pp2d5h&xoj7fnoanmti_9*-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
